c06cf18353a9192510189f7682c934a4a63804c0
arecouz <richardcouzens96@gmail.com>
2024-08-30 17:02:18 +0100

    individual page e2e test added

---
+++ e2e-tests/individualPage.spec.js
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test'
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:8080/')
+  await page.getByRole('link', { name: 'ivysaur' }).click()
+  await page.getByText('chlorophyll').click()
+})
