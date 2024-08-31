4daa697aedea5e99f68215d04fd18d64dd7fbc4f
arecouz <richardcouzens96@gmail.com>
2024-08-31 12:26:39 +0100

    fly health check, error -> ok

--- app.js
+++ app.js
@@ -13,8 +13,6 @@ app.get('/version', (req, res) => {
 
 // For HTTP-level health check
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
   res.send('ok')
 })
 
